98a2ba5a61b324459ec2f5b69e03bae48ff14491
cyrianedub1 <johndoe@email>
2025-11-07 18:16:22 +0100

    J'ai établit un liens entre le fichier javascript et les pages html pour que cela soit plus organisé en fesant un liens.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,11 @@
 /*Un fichier javascript pour plus tard*/
+function buoap(){
+            var buoap = new Audio();
+            buoap.src = "SFX/ROBLOX Sound - button.mp3";
+            buoap.play();
+        }
+        function boooo(){
+            var boooo = new Audio();
+            boooo.src = "SFX/Build (Bass.wav) - Classic Trowel - Roblox.mp3";
+            boooo.play();
+        }
