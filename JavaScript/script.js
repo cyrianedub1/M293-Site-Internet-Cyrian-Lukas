331b8ba44dc40e28e7d4024229c0f0895a3beaaf
Your Name <lukas.gvt@eduge.ch>
2025-11-12 13:58:56 +0100

    Bugs avec le son corrigé Co-authored-by: cyrianedub1 <cyrianedub1@users.noreply.github.com>

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,15 +2,36 @@
 
 /*Script qui permet de faire un sons lorsque l'ont clique sur l'un des liens*/
 function buoap(){
-            var buoap = new Audio();
+            let buoap = new Audio();
             buoap.src = "SFX/ROBLOX Sound - button.mp3";
             buoap.play();
         }
-        function boooo(){
-            var boooo = new Audio();
+function boooo(){
+            let boooo = new Audio();
             boooo.src = "SFX/Build (Bass.wav) - Classic Trowel - Roblox.mp3";
             boooo.play();
         }
+ /*Methode pour la redirection*/       
+function redirection(url){
+    window.location.href = url;
+}
+/*Fin de la methode*/ 
 
+/*Debut de l'événement qui déclanche le son sur le premier lien*/ 
+document.getElementById("premierlien").addEventListener("click", (e) => {
+   
+    buoap();//déclanche le son
+    setTimeout(redirection, 525, "dynablock.html")//
+    
+})
+
+document.getElementById("deuxiemelien").addEventListener("click", (e) => {
+   
+    boooo();//déclanche le son
+    setTimeout(redirection, 600, "dynablock.html")//
+    
+})
 /*Fin*/
+
+
         
