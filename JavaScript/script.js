9e8ed5d49d95fd1327aec78c5356d9d8bdaedc28
lukasghub <lukas.gvt@eduge.ch>
2026-01-03 03:03:00 +0100

    Commit pour voir si le js fonctionne correctement sur la 1ère page

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -44,10 +44,13 @@ document.getElementById("4emelien").addEventListener("click", (e) => {
   boooo(); //déclanche le son
   setTimeout(redirection, 600, "RobloxModern.html"); //Met un temps d'attente de 525ms puis déclanche la fonction 'redirection' qui redirige sur le lien choisi
 });
+document.getElementById("5emelien").addEventListener("click", (e) => {
+  buoap(); //déclanche le son
+  setTimeout(redirection, 600, "RobloxActuel.html"); //Met un temps d'attente de 525ms puis déclanche la fonction 'redirection' qui redirige sur le lien choisi
+});
 
 
-
-
+//Début du code qui fait en sorte que quand tu clique sur le bouton Start les boutons qui dirigent vers les autres pages soient visible
 const toggleButton = document.getElementById("toggleButton");
 const liensContainer = document.getElementById("liensContainer");
 
@@ -55,6 +58,8 @@ toggleButton.addEventListener("click", function () {
   startupSFX();
   liensContainer.classList.toggle("hidden");
 });
+//Fin
+
 /*Fin*/
 
 // Code bonus à voir si on le garde ou pas 
@@ -66,15 +71,15 @@ if (window.location.pathname.endsWith("index.html")) {
   // Affiche une alerte de bienvenue
   alert("Bienvenue sur le site sur l'Histoire de Roblox !");
 }
-
+//si le nom du chemin de la page fini par "dynablock.html" alors une alerte se lance 
 else if(window.location.pathname.endsWith("dynablock.html")){
   alert("Bienvenue dans la section Dynablock !");
 }
-
+//si le nom du chemin de la page fini par "Roblox2006-20l7.html" alors une alerte se lance 
 else if(window.location.pathname.endsWith("Roblox2006-2017.html")){
   alert("Bienvenue dans la section Roblox 2006-2017 !");
 }
-
+//si le nom du chemin de la page fini par "RobloxModern.html" alors une alerte se lance 
 else if(window.location.pathname.endsWith("RobloxModern.html")){
   alert("Bienvenue dans la section Roblox Moderne !");
 }
